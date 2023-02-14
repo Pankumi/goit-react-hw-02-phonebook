2078f64ff28ab75c9f7ad9014d50299f229f9dc8
Pankumi <111710215+Pankumi@users.noreply.github.com>
2023-02-15 00:55:24 +0200

    Step 3

--- src/components/App.js
+++ src/components/App.js
@@ -6,34 +6,36 @@ import { Filter } from './Filter/Filter';
 export class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', userName: 'Rosie Simpson', userNumber: '459-12-56'},
-      {id: 'id-2', userName: 'Hermione Kline', userNumber: '443-89-12'},
-      {id: 'id-3', userName: 'Eden Clements', userNumber: '645-17-79'},
-      {id: 'id-4', userName: 'Annie Copeland', userNumber: '227-91-26'},
+      { id: 'id-1', userName: 'Rosie Simpson', userNumber: '459-12-56' },
+      { id: 'id-2', userName: 'Hermione Kline', userNumber: '443-89-12' },
+      { id: 'id-3', userName: 'Eden Clements', userNumber: '645-17-79' },
+      { id: 'id-4', userName: 'Annie Copeland', userNumber: '227-91-26' },
     ],
     filter: '',
-  }
+  };
 
   addContacts = newContact => {
-    if(this.state.contacts.some(stateContact => stateContact.userName === newContact.userName)){
+    if (
+      this.state.contacts.some(
+        stateContact => stateContact.userName === newContact.userName
+      )
+    ) {
       alert(`${newContact.userName} is already in contacts.`);
-      return
+      return;
     }
 
     this.setState(prevState => {
       return { contacts: [...prevState.contacts, newContact] };
     });
-
     // // or:
     // this.setState(prevState => ({
     //   contacts: [...prevState.contacts, newContact]
     // }));
-
     // // or:
     // this.setState({
     //   contacts: [...this.state.contacts, newContact],
     // });
-  }
+  };
 
   filterChange = evt => {
     const { name, value } = evt.target;
@@ -41,15 +43,25 @@ export class App extends Component {
   };
 
   render() {
-    console.log(this.state);
+    const filterContact = this.state.contacts.filter(contakt =>
+      contakt.userName
+        .toLowerCase()
+        .trim()
+        .includes(this.state.filter.toLocaleLowerCase())
+    );
+    // console.log(filterContact);
+    // console.log(this.state);
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm addContacts={this.addContacts}/>
+        <ContactForm addContacts={this.addContacts} />
 
         <h2>Contacts</h2>
-        <Filter filterValue={this.state.filter} filterChange={this.filterChange}/>
-        <ContactList contacts={this.state.contacts}/>
+        <Filter
+          filterValue={this.state.filter}
+          filterChange={this.filterChange}
+        />
+        <ContactList contacts={filterContact} />
       </div>
     );
   }
